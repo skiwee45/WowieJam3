a3b2239ca8d5b27d892fb6ad4210c89bd4d9edfb
Raymond Lin <raymond.lin.05@gmail.com>
2021-02-26 23:07:40 -0500

    Input manager and gamemanager gameobject

--- WowieJam3/Assets/Scripts/InputManager.cs
+++ WowieJam3/Assets/Scripts/InputManager.cs
@@ -1,18 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
-public class InputManager : MonoBehaviour
+namespace GameManagers
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[Serializable]
+	public class Vector2Event : UnityEvent<Vector2>{}
+	
+	public class InputManager : MonoBehaviour
+	{
+		InputControls controls;
+	
+		//unityevents to call
+		public Vector2Event move;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		// Awake is called when the script instance is being loaded.
+		protected void Awake()
+		{
+			controls = new InputControls();
+			EnableGameplayMap(true);
+			//sign up methods
+		}
+
+		//special method to enable / disable maps
+		public void EnableGameplayMap(bool enable)
+		{
+			if (enable)
+			{
+				controls.Gameplay.Enable();
+			} else
+			{
+				controls.Gameplay.Disable();
+			}
+		}
+	
+		public void MoveInput(InputAction.CallbackContext value)
+		{
+			var input = value.ReadValue<Vector2>();
+			move?.Invoke(input);
+		}
+	}
 }
